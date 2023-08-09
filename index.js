cbaec40ccdf3ee46fc06104e8f14fcd878ec3643
Leojoy62 <leojoy62@gmail.com>
2023-08-10 00:30:28 +0600

    added

--- index.js
+++ index.js
@@ -84,9 +84,6 @@ async function run() {
       console.log(sort);
       const search = req.query.search;
 
-      // const query = {};
-      // const query = { price: {$gte: 50, $lte:150}};
-      // db.InspirationalWomen.find({first_name: { $regex: /Harriet/i} })
       const query = { title: { $regex: ".*" + search + ".*", $options: "i" } };
       const options = {
         // sort matched documents in descending order by rating
